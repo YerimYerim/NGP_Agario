57fe918412f461bf5d02afc1ec1324ce3237d79f
DESKTOP-ND36B9N\rabit <Vsteryelim79513!>
2020-11-08 14:17:04 +0900

    add Class
    add Class

---
+++ client/NGP_Agario_Client/CircleObject.cpp
@@ -0,0 +1,14 @@
+#include "CircleObject.h"
+
+void CircleObject::SetPosition()
+{
+}
+
+Position CircleObject::GetPosition()
+{
+    return Position();
+}
+
+void CircleObject::SetSize(int nsize)
+{
+}
