23a87ffcd43585571c5a0cf6750c703976a62074
DESKTOP-ND36B9N\rabit <Vsteryelim79513!>
2020-12-01 12:48:27 +0900

    name change

--- sever/NGP_Agario_Server - 이예림/client/NGP_Agario_Client/NGP_Agario_Client.cpp
+++ sever/NGP_Agario_Server - 이예림/client/NGP_Agario_Client/NGP_Agario_Client.cpp
@@ -25,7 +25,7 @@ HANDLE FindNewPlayer;
 SendDirection sendDirection;
 // 전역 변수:
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szTitle[MAX_LOADSTRING] = { L"Sever" };                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 CircleObject Circle;
 Player player;
@@ -256,9 +256,9 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
-
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       500, 200, 800, 800, nullptr, nullptr, hInstance, nullptr);
+   SetWindowText(hWnd, { "Sever" });
 
    if (!hWnd)
    {
